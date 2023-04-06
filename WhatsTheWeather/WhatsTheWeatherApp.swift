3cf37a276b90aaa4dfff699653f756bd199a9b6c
aselka27 <asjumalieva@gmail.com>
2023-04-06 17:27:04 -0500

    Initial Commit

---
+++ WhatsTheWeather/WhatsTheWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsTheWeatherApp.swift
+//  WhatsTheWeather
+//
+//  Created by саргашкаева on 6.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatsTheWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
